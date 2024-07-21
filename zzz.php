c5a62f2198931a0665539bccd5aa659f900e47d5
i-qof <muhammadfadly.mfd@gmail.com>
2024-07-21 10:04:19 +0700

    feat(template): pembuatan template moderenize

---
+++ zzz.php
@@ -0,0 +1,11 @@
+<?php
+// include "head.php";
+include "templete/head.php";
+include "templete/sidebar.php";
+?>
+<?php
+include "templete/script.php";
+?>
+<?php
+include "templete/footer.php";
+?>
